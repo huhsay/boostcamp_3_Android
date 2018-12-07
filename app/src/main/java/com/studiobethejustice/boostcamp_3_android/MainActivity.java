0a446c47a2cbfe3ce9985f848c4197d3ee26c805
Theodore <bethejustice@naver.com>
2018-12-08 00:12:51 +0900

    add: update naver search api conncetion

--- app/src/main/java/com/studiobethejustice/boostcamp_3_android/MainActivity.java
+++ app/src/main/java/com/studiobethejustice/boostcamp_3_android/MainActivity.java
@@ -1,18 +1,22 @@
 package com.studiobethejustice.boostcamp_3_android;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
 
+    final String SEARCH_URL = "https://openapi.naver.com/v1/search/movie?query=";
 
     EditText mTextSearch;
     Button mButtonSearch;
@@ -35,10 +39,43 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
 
                 String searchWord = mTextSearch.getText().toString().trim();
-                SearchThread searchThread = new SearchThread(searchWord);
-                searchThread.start();
+                SearchTask searchTask = new SearchTask(SEARCH_URL, searchWord);
+                searchTask.execute();
 
             }
         });
+    }
+
+    private class SearchTask extends AsyncTask<Void, Void, String> {
+
+        String searchURL;
+        String searchWord;
+
+        public SearchTask(String searchURL, String searchWord) {
+            this.searchURL = searchURL;
+            this.searchWord = searchWord;
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+
+            String result;
+            SearchThread searchThread = new SearchThread();
+            result = searchThread.request(searchURL, searchWord);
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            resultProcess(s);
+        }
+    }
+
+    private void resultProcess(String s) {
+        Gson gson = new Gson();
+
+        Toast.makeText(this, s, Toast.LENGTH_LONG).show();
     }
 }
--- app/src/main/java/com/studiobethejustice/boostcamp_3_android/SearchThread.java
+++ app/src/main/java/com/studiobethejustice/boostcamp_3_android/SearchThread.java
@@ -2,39 +2,30 @@ package com.studiobethejustice.boostcamp_3_android;
 
 import android.util.Log;
 
-import com.android.volley.Request;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.lang.Thread;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
 
-public class SearchThread extends Thread {
+public class SearchThread{
     private static final String TAG = "SearchThread";
-    final String SEARCH_URL = "https://openapi.naver.com/v1/search/movie?query=";
     final String clientId = "nkAhHANDxYvZUtA3dE06";
     final String clientSecret = "q5D_4UF3vo";
 
     String searchWord;
+    String searchURL;
+    StringBuffer response;
 
 
-    public SearchThread(String searchWord) {
+    public String request(String searchURL, String searchWord) {
+
+        this.searchURL = searchURL;
         this.searchWord = searchWord;
-    }
-
-    @Override
-    public void run() {
-
-        Log.d(TAG, "sendRequest: started "+ searchWord);
 
         try {
             String encodedWord = URLEncoder.encode(searchWord, "UTF-8");
-            String apiURL = SEARCH_URL + encodedWord;
+            String apiURL = searchURL + encodedWord;
 
             URL url = new URL(apiURL);
             HttpURLConnection con = (HttpURLConnection) url.openConnection();
@@ -52,14 +43,18 @@ public class SearchThread extends Thread {
                 br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
             }
             String inputLine;
-            StringBuffer response = new StringBuffer();
+            response = new StringBuffer();
             while ((inputLine = br.readLine()) != null) {
                 response.append(inputLine);
             }
             br.close();
             System.out.println(response.toString());
+
+
         } catch (Exception e) {
             System.out.println(e);
         }
+
+        return response.toString();
     }
 }
